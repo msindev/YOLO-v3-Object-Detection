39d5048820d84e139b148fd26c48d9a7284657c1
Mohit Singh <mohitwildbeast@gmail.com>
2018-12-23 11:56:17 +0530

    Immport necessary dependencies, and all all layers input etc

---
+++ yolo_detection_images.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+
+modelConfiguration = 'cfg/yolov3.cfg'
+modelWeights = 'yolov3.weights'
+
+labelsPath = 'coco.names'
+labels = open(labelsPath).read().strip().split('\n')
+
+net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+
+image = 'images/dog.jpg'
+(H, W) = image.shape[:2]
+
+#Determine output layer names
+layerName = net.getLayerNames()
+layerName = [layerName[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB = True, crop = False)
