ea12b2c024d3235625afa3b590f27d124d953b3c
Mohit Singh <mohitwildbeast@gmail.com>
2018-12-23 18:29:15 +0530

    Initialize model path, and configuration path

--- yolo_detection_video.py
+++ yolo_detection_video.py
@@ -1,2 +1,14 @@
 import numpy as np
 import cv2
+
+confidenceThreshold = 0.5
+NMSThreshold = 0.3
+
+modelConfiguration = 'cfg/yolov3.cfg'
+modelWeights = 'yolov3.weights'
+
+labelsPath = 'coco.names'
+labels = open(labelsPath).read().strip().split('\n')
+
+np.random.seed(10)
+COLORS = np.random.randint(0, 255, size = (len(labels), 3), dtype = 'unit8')
