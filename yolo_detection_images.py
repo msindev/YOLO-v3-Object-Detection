483ebf164e21448e1fa45c0248c97fb92df770ff
Mohit Singh <mohitwildbeast@gmail.com>
2018-12-23 13:12:00 +0530

    Fixed random color problem

--- yolo_detection_images.py
+++ yolo_detection_images.py
@@ -4,14 +4,14 @@ import cv2
 confidenceThreshold = 0.5
 NMSThreshold = 0.3
 
-np.random.seed(42)
-COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
-
 modelConfiguration = 'cfg/yolov3.cfg'
 modelWeights = 'yolov3.weights'
 
 labelsPath = 'coco.names'
 labels = open(labelsPath).read().strip().split('\n')
+
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
 
 net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
 
