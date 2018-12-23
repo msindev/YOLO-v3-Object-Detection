e257609209088dea2545de729c655cbb30d53b26
Mohit Singh <mohitwildbeast@gmail.com>
2018-12-23 19:25:29 +0530

    YOLO Object detection for videos

--- yolo_detection_video.py
+++ yolo_detection_video.py
@@ -12,3 +12,69 @@ labels = open(labelsPath).read().strip().split('\n')
 
 np.random.seed(10)
 COLORS = np.random.randint(0, 255, size = (len(labels), 3), dtype = 'unit8')
+
+net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+
+outputLayer = net.getLayerNames()
+outputLayer = [outputLayer[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+video = cv2.VideoCapture('video.mp4')
+writer = None
+(W, H) = (None, None)
+
+try:
+    prop = cv2.CAP_PROP_FRAME_COUNT
+    total = int(video.get(prop))
+    print("[INFO] {} total frames in video".format(total))
+except:
+    printf("Could not determine no. of frames in video")
+
+while True:
+    (ret, frame) = video.read()
+    if not ret:
+        break
+    if W is None or H is None:
+        (H,W) = frame.shape[:2]
+
+        blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB = True, crop = False)
+        net.setInput(blob)
+        layersOutputs = net.forward(outputLayer)
+
+        boxes = []
+        confidences = []
+        classIDs = []
+
+        for output in layersOutputs:
+            for detection in output:
+                scores = detection[5:]
+                classID = np.argmax(scores)
+                confidence = scores[classID]
+                if confidence > confidenceThreshold:
+                    box = detection[0:4] * np.array([W, H, W, H])
+                    (centerX, centerY,  width, height) = box.astype('int')
+                    x = int(centerX - (width/2))
+                    y = int(centerY - (height/2))
+
+                    boxes.append([x, y, int(width), int(height)])
+                    confidences.append(float(confidence))
+                    classIDs.append(classID)
+
+        #Apply Non Maxima Suppression
+        detectionNMS = cv2.dnn.NMSBoxes(boxes, confidences, confidenceThreshold, NMSThreshold)
+        if(len(detectionNMS) > 0):
+            for i in detectionNMS.flatten():
+                (x, y) = (boxes[i][0], boxes[i][1])
+                (w, h) = (boxes[i][2], boxes[i][3])
+
+                color = [int(c) for c in COLORS[classIDs[i]]]
+                cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+                text = '{}: {:.4f}'.format(labels[classIDs[i]], confidences[i])
+                cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+                if writer is None:
+                    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+                    writer = cv2.VideoWriter('output.mp4', fourcc, 30, (frame.shape[1], frame.shape[0]), True)
+                writer.write(frame)
+
+writer.release()
+video.release()
