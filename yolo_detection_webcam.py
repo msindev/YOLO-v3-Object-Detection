3411eff3d36beb792271ebbd2f7a17c14d34dc05
Mohit Singh <mohitwildbeast@gmail.com>
2019-01-03 19:44:53 +0530

    Add model and label path

--- yolo_detection_webcam.py
+++ yolo_detection_webcam.py
@@ -1,2 +1,19 @@
 import numpy as np
 import cv2
+
+confidenceThreshold = 0.5
+NMSThreshold = 0.3
+
+modelConfiguration = 'cfg/yolov3.cfg'
+modelWeights = 'yolov3.weights'
+
+labelsPath = 'coco.names'
+labels = open(labelsPath).read().strip().split('\n')
+
+np.random.seed(10)
+COLORS = np.random.randint(0, 255, size=(len(labels), 3), dtype="uint8")
+
+net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+
+outputLayer = net.getLayerNames()
+outputLayer = [outputLayer[i[0] - 1] for i in net.getUnconnectedOutLayers()]
