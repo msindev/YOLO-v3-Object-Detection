c3b602c52e1e7ee69195dec782f625b785e463b9
Mohit Singh <mohitwildbeast@gmail.com>
2019-01-03 19:49:31 +0530

    Webcam object detection complete

--- yolo_detection_webcam.py
+++ yolo_detection_webcam.py
@@ -17,3 +17,56 @@ net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
 
 outputLayer = net.getLayerNames()
 outputLayer = [outputLayer[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+video_capture = cv2.VideoCapture(0)
+
+(W, H) = (None, None)
+
+while True:
+    ret, frame = video_capture.read()
+    frame = cv2.flip(frame, 1)
+    if W is None or H is None:
+        (H,W) = frame.shape[:2]
+
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB = True, crop = False)
+    net.setInput(blob)
+    layersOutputs = net.forward(outputLayer)
+
+    boxes = []
+    confidences = []
+    classIDs = []
+
+    for output in layersOutputs:
+        for detection in output:
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+            if confidence > confidenceThreshold:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY,  width, height) = box.astype('int')
+                x = int(centerX - (width/2))
+                y = int(centerY - (height/2))
+
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+
+    #Apply Non Maxima Suppression
+    detectionNMS = cv2.dnn.NMSBoxes(boxes, confidences, confidenceThreshold, NMSThreshold)
+    if(len(detectionNMS) > 0):
+        for i in detectionNMS.flatten():
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+
+            color = [int(c) for c in COLORS[classIDs[i]]]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            text = '{}: {:.4f}'.format(labels[classIDs[i]], confidences[i])
+            cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    cv2.imshow('Output', frame)
+    if(cv.waitKey(1) & 0xFF == ord('q')):
+        break
+
+#Finally when video capture is over, release the video capture and destroyAllWindows
+video_capture.release()
+cv2.destroyAllWindows()
